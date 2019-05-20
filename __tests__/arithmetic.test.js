1ff1b5657a3fd6e6800bd8d90038023e6d339602
Austin Jess <ajess33@gmail.com>
2019-05-20 16:45:03 -0500

    added multiply and divide logic and created tests for each method

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -2,3 +2,30 @@
 
 const arithmetic = require('../lib/arithmetic.js');
 
+// add
+describe('inputs should be added together', () => {
+  it('addition', () => {
+    expect(arithmetic.add(1, 3)).toEqual(4);
+  });
+});
+
+// subtract
+describe('inputs should be subtracted', () => {
+  it('subtraction', () => {
+    expect(arithmetic.subtract(1, 3)).toEqual(-2);
+  });
+});
+
+// multiply
+describe('inputs should be multiplied', () => {
+  it('multiplication', () => {
+    expect(arithmetic.multiply(1, 3)).toEqual(3);
+  });
+});
+
+// divide
+describe('inputs should be divided', () => {
+  it('division', () => {
+    expect(arithmetic.divide(3, 1)).toEqual(3);
+  });
+});
