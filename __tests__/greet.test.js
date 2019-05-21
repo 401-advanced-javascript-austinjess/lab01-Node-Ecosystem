c5821b542e7b9b071d67ee926c973444ec62d9cd
Austin Jess <ajess33@gmail.com>
2019-05-20 22:13:16 -0500

    added faker data for greet tests

--- __tests__/greet.test.js
+++ __tests__/greet.test.js
@@ -1,9 +1,25 @@
 'use strict';
 
 const greet = require('../lib/greet.js');
+const faker = require('faker');
+
+const randomName = faker.name.firstName();
 
 describe('User should be greeted by name', () => {
   it('greet user', () => {
-    expect(greet('JOHN')).toEqual('Hello JOHN');
+    expect(greet(randomName)).toEqual(`Hello ${randomName}`);
+  });
+});
+
+describe('Input must be a string', () => {
+  it('value is not a string', () => {
+    expect(greet(1)).toEqual(null);
+  });
+});
+
+// write a test that expects 'Hello world' when using world as the input
+describe('hello world test', () => {
+  it('hello world', () => {
+    expect(greet('world')).toEqual(`Hello world`);
   });
 });
